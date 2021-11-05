651ded9703f6bc317022c0933a23be337b71d6fe
Peter Bui <pbui@bx612.space>
2021-11-04 23:29:16 -0400

    Add ergo

---
+++ ergo.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+PKGNAME=ergo
+VERSION=2.7.0
+WORKDIR=$(realpath $PKGNAME)
+MAKEDEPENDS=""
+TARBALL=$PKGNAME-$VERSION.tar.xz
+DISTFILE=https://github.com/ergochat/$PKGNAME/releases/download/v$VERSION/$PKGNAME-$VERSION-linux-x86_64.tar.gz
+
+prepare() {
+    mkdir -p $WORKDIR
+}
+
+fetch() {
+    curl -L $DISTFILE > $WORKDIR/$TARBALL
+}
+
+extract() {
+    cd $WORKDIR
+    tar xvf $TARBALL
+}
+
+depends() {
+    :
+}
+
+build() {
+    cd $WORKDIR/$PKGNAME-$VERSION-linux-x86_64
+}
+
+install() {
+    sudo install -m 755 ergo /usr/local/bin/ergo
+    sudo mkdir -p /usr/local/share/ergo
+    sudo rsync -av --progress languages /usr/local/share/ergo/.
+}
+
+cleanup() {
+    rm -fr $WORKDIR
+}
+
+prepare	&& \
+fetch   && \
+extract && \
+depends && \
+build   && \
+install && \
+cleanup
