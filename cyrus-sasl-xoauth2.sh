3a8006e694630a2e57a3891135e0fdb89fc7eb7d
Peter Bui <pbui@bx612.space>
2021-11-14 09:45:27 -0500

    cyrus-sasl-xoauth2: use ubs wrapper script

--- cyrus-sasl-xoauth2.sh
+++ cyrus-sasl-xoauth2.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 PKGNAME=cyrus-sasl-xoauth2
 VERSION=0.2
 WORKDIR=$(realpath $PKGNAME)
@@ -7,25 +5,8 @@ MAKEDEPENDS="autoconf libsasl2-dev libtool"
 TARBALL=$PKGNAME-$VERSION.tar.gz
 DISTFILE=https://github.com/moriyoshi/$PKGNAME/archive/refs/tags/v$VERSION.tar.gz
 
-prepare() {
-    mkdir -p $WORKDIR
-}
-
-fetch() {
-    curl -L $DISTFILE > $WORKDIR/$TARBALL
-}
-
-extract() {
-    cd $WORKDIR
-    tar xvf $TARBALL
-}
-
-depends() {
-    sudo apt install -y $MAKEDEPENDS
-}
-
 build() {
-    cd $WORKDIR/$PKGNAME-$VERSION
+    cd $SRCDIR
     ./autogen.sh
     ./configure --prefix=/usr/local --sysconfdir=/etc
     make
@@ -36,16 +17,3 @@ install() {
     sudo rsync -av --progress /usr/lib/sasl2/. /usr/lib/x86_64-linux-gnu/sasl2/.
     sudo rm -fr /usr/lib/sasl2
 }
-
-cleanup() {
-    sudo apt autoremove -y $MAKEDEPENDS
-    rm -fr $WORKDIR
-}
-
-prepare	&& \
-fetch   && \
-extract && \
-depends && \
-build   && \
-install && \
-cleanup
