8e2b970350e2f5e0b413d29ef089bfaca3f791b3
Peter Bui <pbui@bx612.space>
2021-11-14 09:41:52 -0500

    bitlbee-discord: use ubs wrapper script

--- bitlbee-discord.sh
+++ bitlbee-discord.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 PKGNAME=bitlbee-discord
 VERSION=0.4.3
 WORKDIR=$(realpath $PKGNAME)
@@ -7,43 +5,9 @@ MAKEDEPENDS="autoconf libtool pkg-config bitlbee-dev libglib2.0-dev"
 TARBALL=$PKGNAME-$VERSION.tar.gz
 DISTFILE=https://github.com/sm00th/$PKGNAME/archive/refs/tags/$VERSION.tar.gz
 
-prepare() {
-    mkdir -p $WORKDIR
-}
-
-fetch() {
-    curl -L $DISTFILE > $WORKDIR/$TARBALL
-}
-
-extract() {
-    cd $WORKDIR
-    tar xvf $TARBALL
-}
-
-depends() {
-    sudo apt install -y $MAKEDEPENDS
-}
-
 build() {
-    cd $WORKDIR/$PKGNAME-$VERSION
+    cd $SRCDIR
     ./autogen.sh
     ./configure --prefix=/usr
     make
 }
-
-install() {
-    sudo make install
-}
-
-cleanup() {
-    sudo apt autoremove -y $MAKEDEPENDS
-    rm -fr $WORKDIR
-}
-
-prepare	&& \
-fetch   && \
-extract && \
-depends && \
-build   && \
-install && \
-cleanup
