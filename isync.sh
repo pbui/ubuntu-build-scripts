592fe55963ed92ab36d0f69fb087d48d94d15b73
Peter Bui <pbui@bx612.space>
2021-11-14 09:38:38 -0500

    isync: use ubs wrapper script

--- isync.sh
+++ isync.sh
@@ -1,48 +1,7 @@
-#!/bin/sh
-
 PKGNAME=isync
 VERSION=1.4.3
 WORKDIR=$(realpath $PKGNAME)
 MAKEDEPENDS="libdb-dev libsasl2-dev libssl-dev zlib1g-dev "
 TARBALL=$PKGNAME-$VERSION.tar.gz
 DISTFILE=https://iweb.dl.sourceforge.net/project/$PKGNAME/$PKGNAME/$VERSION/$TARBALL
-
-prepare() {
-    mkdir -p $WORKDIR
-}
-
-fetch() {
-    curl -L $DISTFILE > $WORKDIR/$TARBALL
-}
-
-extract() {
-    cd $WORKDIR
-    tar xvf $TARBALL
-}
-
-depends() {
-    sudo apt install -y $MAKEDEPENDS
-}
-
-build() {
-    cd $WORKDIR/$PKGNAME-$VERSION
-    ./configure --prefix=/usr/local --sysconfdir=/etc
-    make
-}
-
-install() {
-    sudo make install
-}
-
-cleanup() {
-    sudo apt autoremove -y $MAKEDEPENDS
-    rm -fr $WORKDIR
-}
-
-prepare	&& \
-fetch   && \
-extract && \
-depends && \
-build   && \
-install && \
-cleanup
+CONFIGURE_ARGS="--prefix=/usr/local --sysconfdir=/etc"
