29d715fbcce4b00d165c195170f5d387034e683b
Peter Bui <pbui@bx612.space>
2022-01-22 16:33:50 -0500

    gotop: install directory

--- gotop.sh
+++ gotop.sh
@@ -3,7 +3,6 @@ VERSION=4.1.2
 WORKDIR=$(realpath $PKGNAME)
 SRCDIR=$WORKDIR
 MAKEDEPENDS=""
-TARBALL=$PKGNAME-$VERSION.tgz
 DISTFILE=https://github.com/xxxserxxx/$PKGNAME/releases/download/v$VERSION/${PKGNAME}_v${VERSION}_linux_amd64.tgz
 
 build() {
