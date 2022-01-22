200e612c6aca92387a37e54fc4fe9bc4188de391
Peter Bui <pbui@bx612.space>
2022-01-22 16:38:47 -0500

    isync: update to 1.4.4, drop TARBALL

--- isync.sh
+++ isync.sh
@@ -1,7 +1,6 @@
 PKGNAME=isync
-VERSION=1.4.3
+VERSION=1.4.4
 WORKDIR=$(realpath $PKGNAME)
 MAKEDEPENDS="libdb-dev libsasl2-dev libssl-dev zlib1g-dev "
-TARBALL=$PKGNAME-$VERSION.tar.gz
-DISTFILE=https://iweb.dl.sourceforge.net/project/$PKGNAME/$PKGNAME/$VERSION/$TARBALL
+DISTFILE=https://iweb.dl.sourceforge.net/project/$PKGNAME/$PKGNAME/$VERSION/$PKGNAME-$VERSION.tar.gz
 CONFIGURE_ARGS="--prefix=/usr/local --sysconfdir=/etc"
