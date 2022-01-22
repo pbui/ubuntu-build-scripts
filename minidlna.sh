1fcc07548334dffce686d9791e22524edd136071
Peter Bui <pbui@bx612.space>
2022-01-22 16:46:07 -0500

    minidlna: add CONFIGURE_ARGS

--- minidlna.sh
+++ minidlna.sh
@@ -3,3 +3,4 @@ VERSION=1.3.0
 WORKDIR=$(realpath $PKGNAME)
 MAKEDEPENDS="libavcodec-dev libavformat-dev libflac-dev libexif-dev libid3tag0-dev libjpeg-dev libvorbis-dev libsqlite3-dev gettext"
 DISTFILE=https://iweb.dl.sourceforge.net/project/$PKGNAME/$PKGNAME/$VERSION/$PKGNAME-$VERSION.tar.gz
+CONFIGURE_ARGS="--prefix=/usr/local --sbindir=/usr/local/bin"
