985f2d8ee0db35536dd924050f6ca5fa48cdd90e
Peter Bui <pbui@bx612.space>
2022-01-22 17:00:50 -0500

    makemkv: broken

--- makemkv.sh
+++ makemkv.sh
@@ -4,6 +4,29 @@ WORKDIR=$(realpath $PKGNAME)
 MAKEDEPENDS="libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev qtbase5-dev zlib1g-dev"
 DISTFILE="https://www.makemkv.com/download/makemkv-bin-$VERSION.tar.gz
   https://www.makemkv.com/download/makemkv-oss-$VERSION.tar.gz"
-CONFIGURE_ARGS="--prefix=/usr/local"
+
+configure() {
+    cd $WORKDIR/makemkv-oss-$VERSION
+    ./configure $CONFIGURE_ARGS
+
+    cd $WORKDIR/makemkv-bin-$VERSION
+    ./configure $CONFIGURE_ARGS
+}
+
+build() {
+    cd $WORKDIR/makemkv-oss-$VERSION
+    make
+
+    cd $WORKDIR/makemkv-bin-$VERSION
+    make
+}
+
+install() {
+    cd $WORKDIR/makemkv-oss-$VERSION
+    sudo make PREFIX=/usr/local install
+    
+    cd $WORKDIR/makemkv-bin-$VERSION
+    sudo make PREFIX=/usr/local install
+}
 
 # https://forum.makemkv.com/forum/viewtopic.php?f=3&t=224
