382b023830d2376cac6bc3f73a13aeccbafe1210
Peter Bui <pbui@bx612.space>
2022-04-30 12:50:23 -0400

    msmtp: update to 1.8.20
    Add libtool dependency to pull in compiler

--- msmtp.sh
+++ msmtp.sh
@@ -1,6 +1,6 @@
 PKGNAME=msmtp
-VERSION=1.8.19
+VERSION=1.8.20
 WORKDIR=$(realpath $PKGNAME)
-MAKEDEPENDS="libssl-dev pkg-config libgnutls28-dev"
+MAKEDEPENDS="autoconf libtool libssl-dev pkg-config libgnutls28-dev"
 DISTFILE=https://marlam.de/$PKGNAME/releases/$PKGNAME-$VERSION.tar.xz
 CONFIGURE_ARGS="--prefix=/usr/local --sysconfdir=/etc"
