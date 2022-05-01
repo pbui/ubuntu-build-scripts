bfeda7ea97cc55cdbd9829bfd757a1ecaffafa73
Peter Bui <pbui@bx612.space>
2022-05-01 12:59:53 -0400

    bitlbee-discord: add patch for glib >= 2.68

--- bitlbee-discord.sh
+++ bitlbee-discord.sh
@@ -3,6 +3,7 @@ VERSION=0.4.3
 WORKDIR=$(realpath $PKGNAME)
 MAKEDEPENDS="autoconf libtool pkg-config bitlbee-dev libglib2.0-dev"
 DISTFILE=https://github.com/sm00th/$PKGNAME/archive/refs/tags/$VERSION.tar.gz
+PATCHES="https://github.com/sm00th/bitlbee-discord/commit/607f9887ca85f246e970778e3d40aa5c346365a7.patch"
 
 build() {
     cd $SRCDIR
